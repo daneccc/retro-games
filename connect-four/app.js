9f0b50f7102c1a612a737892da04f588c6b5cffe
daneccf <danielecavalcante1@hotmail.com>
2020-04-24 22:20:53 -0300

    app

--- connect-four/app.js
+++ connect-four/app.js
@@ -1,1 +1,13 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', () => {
+    cont squares = document.querySelectorAll('.grid div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player')
+    let currentPlayer = 1
+
+    for (var i = 0, len = squares.length; i < len; i++)
+
+    //parei em 24min
+
+
+
+})
