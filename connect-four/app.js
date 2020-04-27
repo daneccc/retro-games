841538a496d3fe6c4d4b223ac3460ba4bacfb812
daneccf <danielecavalcante1@hotmail.com>
2020-04-26 21:26:24 -0300

    final

--- connect-four/app.js
+++ connect-four/app.js
@@ -6,11 +6,11 @@
 
     for (var i = 0, len = squares.length; i < len; i++)
 
-    (function(index){
-    //add an onclick to each square in your grid
-        squares[i].onclick = function(){
+    (function(index) {
+        //add an onclick to each square in your grid
+        squares[i].onclick = function() {
             //if the square below your current square is taken, you can go ontop of it
-            if(squares[index + 7].classList.contains('taken')){
+            if (squares[index + 7].classList.contains('taken')) {
                 if (currentPlayer === 1) {
                     squares[index].classList.add('taken')
                     squares[index].classList.add('player-one')
@@ -24,10 +24,10 @@
                     currentPlayer = 1
                     displayCurrentPlayer.innerHTML = currentPlayer
                 }
-                //if the sqaure below your current swqaure is not taken, you can't go there
-                } else alert('cant go here')
-            }
-    })(i)
+                //if the square below your current square is not taken, you can't go here
+                } else alert('can\'t go here')
+        }
+    }) (i)
 
     //check the board for a win or lose
     function checkBoard() {
@@ -44,34 +44,33 @@
         [22, 23, 24, 25], [26, 25, 24, 23], [29, 30, 31, 32], [33, 32, 31, 30], [36, 37, 38, 39], [40, 39, 38, 37], [7, 14, 21, 28],
         [8, 15, 22, 29], [9, 16, 23, 30], [10, 17, 24, 31], [11, 18, 25, 32], [12, 19, 26, 33], [13, 20, 27, 34]
         ];
-        //now take the 4 values in earch winningArray & plug them into the squares values 
+        //now take the 4 values in each winningArrays and plug then into the squares
         for(let y = 0; y < winningArrays.length; y++) {
             const square1 = squares[winningArrays[y][0]];
             const square2 = squares[winningArrays[y][1]];
             const square3 = squares[winningArrays[y][2]];
             const square4 = squares[winningArrays[y][3]];
-
-            //now check those arrays to see if they all have the class of player-one
-            if(square1.classList.contains('player-one') &&
+             
+            //now check those arrays to see if they all have the class of player-one    
+            if (square1.classList.contains('player-one') &&
                 square2.classList.contains('player-one') &&
                 square3.classList.contains('player-one') &&
                 square4.classList.contains('player-one')) {
                     //if they do, player-one is passed as the winner
-                    result.innerHTML = 'Player one wins!'
-                    //remove ability to change result
-            } 
-            //now check to see if they all have the classname player two
+                    result.innerHTML = 'Player One Wins!'
+                }
+                //now check to see if they all have the class name of player-two
             else if (square1.classList.contains('player-two') &&
                 square2.classList.contains('player-two') &&
                 square3.classList.contains('player-two') &&
                 square4.classList.contains('player-two')) {
-                    //if they do, player-two is passed as the winner as well as the chip positions
-                    result.innerHTML = 'Player two wins!'
-            }
+                //if they do, player-two is passed on as the winner
+                result.innerHTML = 'Player Two Wins!'
+            }   
         }
-    }
+    }   
 
-    //add an event listener to each square that will trigger the checkBoard function on click
+    //add an eventListener to each square that will triggere the checkBoard function on click
     squares.forEach(square => square.addEventListener('click', checkBoard))
 
 })
